c5d433fa068540cee39eb587230a183602b2c477
Hasta Budiawan <hastabudiawan9@gmail.com>
2026-06-13 15:12:27 +0700

    modified:   sanity/lib/live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,9 +1,19 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import { client } from './client'
 
 const builder = createImageUrlBuilder(client)
 
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: Parameters<typeof builder.image>[0]) {
   return builder.image(source)
 }
+
+
+// import createImageUrlBuilder from '@sanity/image-url'
+// import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+// import { client } from './client'
+
+// const builder = createImageUrlBuilder(client)
+
+// export function urlFor(source: SanityImageSource) {
+//   return builder.image(source)
+// }
